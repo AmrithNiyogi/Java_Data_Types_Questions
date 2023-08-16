03a405f7c78e01e7f7a7e0305997b301d1c773f3
AmrithNiyogi <niyogiamrith@gmail.com>
2023-08-16 10:54:52 +0530

    Inches to Meter Conversion

--- src/InchesMeterConversion.java
+++ src/InchesMeterConversion.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 public class InchesMeterConversion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number of inches: ");
+        float inches = sc.nextFloat();
+
+        float meters = inches * 0.0254F;
+
+        System.out.println(inches + " inches = " + meters + " meters");
     }
 }
